658eaddb51539bd7be19c0464e5b22484c1c84a0
Nikhil Kshirsagar <nkjava2050@gmail.com>
2019-03-10 18:45:29 +0530

    Update AutopumpingWithFloatSwitch.ino
    Update AutopumpingWithFloatSwitch pgm for 2 float switches in top waterstorage

--- AutopumpingWithFloatSwitch.ino
+++ AutopumpingWithFloatSwitch.ino
@@ -3,7 +3,7 @@
 
 #define USERNAME "nkjava2050"
 #define DEVICE_ID "TestNMCU2"
-#define DEVICE_CREDENTIAL "someSecretKey"
+#define DEVICE_CREDENTIAL "someSecretKey2"
 
 #define SSID "D-Link_DIR-81"
 #define SSID_PASSWORD "Nikhil"
@@ -12,18 +12,20 @@ ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
 void setup() {
   pinMode(D4, OUTPUT);                // Motor/LED
-  pinMode(D5, OUTPUT);                // For MotorMode variable
   
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
+  // OUTPUT
   thing["motor"] << digitalPin(D4);
-  thing["motorMode"] << digitalPin(D5);   // 0:Manual, 1:Automatic
 
   // resource output example (i.e. reading a sensor value)
+  // INPUT
   thing["footStorageBaseFloat"] >> outputValue(digitalRead(D0));
-  // thing["headStorageBaseFloat"] >> outputValue(digitalRead(D1));
+  thing["headStorageBaseFloat"] >> outputValue(digitalRead(D1));
   thing["headStorageTopFloat"] >> outputValue(digitalRead(D2));
+  thing["motorModeBtn"] >> outputValue(digitalRead(D5));  // 0:Manual, 1:Automatic
+  thing["motorBtn"] >> outputValue(digitalRead(D6));
   
   // thing["millis"] >> outputValue(millis());
   // more details at http://docs.thinger.io/arduino/
@@ -34,14 +36,22 @@ void loop() {
 
   if(digitalRead(D5)==HIGH) {   // If motorMode=Auto
     if(digitalRead(D0)==LOW){   // footStorage is not Empty
-      if(digitalRead(D2)==HIGH){ // headStorage is not Full
-        digitalWrite(D4, HIGH);   // Start motor
-      } else {                  // headStorage is Full
-        digitalWrite(D4, LOW);    // Stop motor
+      
+      if(digitalRead(D2)==LOW){         // headStorage is Full
+        digitalWrite(D4, LOW);              // Stop motor
+      } else if(digitalRead(D1)==HIGH){ // headStorage is not Full
+        digitalWrite(D4, HIGH);             // Start motor
       }
     
-    } else {                    // footStorage is Empty
-      digitalWrite(D4, LOW);      // Stop motor
+    } else {                            // footStorage is Empty
+      digitalWrite(D4, LOW);                // Stop motor
+    }
+  
+  } else {                      // If motorMode=Manual
+    if(digitalRead(D6) == HIGH){
+      digitalWrite(D4, HIGH); 
+    } else {
+      digitalWrite(D4, LOW);       
     }
   }
 }
